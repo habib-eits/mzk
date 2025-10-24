95e22b361fc75e2968e13ab205c965d57d353fa0
Habib EITS <habib@eits.ae>
2025-10-24 16:49:49 +0500

    Migration fixed

---
+++ database/migrations/2025_10_24_114910_extend_invoice_detail_schema.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::table('invoice_detail', function (Blueprint $table) {
+            $table->decimal('Previous',15,3)->nullable();
+            $table->decimal('Current',15,3)->nullable();
+            $table->decimal('Cumulative',15,3)->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('invoice_detail', function (Blueprint $table) {
+            $table->dropColumn([
+                'Previous',
+                'Current',
+                'Cumulative',
+            ]);
+
+        });
+    }
+};
